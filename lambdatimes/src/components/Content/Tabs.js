78dea217a15ab4ed8a1a57d99b1bb65365b4e0c2
Awa Melvine <melvineawa@gmail.com>
2019-06-07 10:47:00 +0100

    Use styled component on Tabs component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,13 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
-
 import Tab from "./Tab";
+
+const TabsContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const Topics = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const Title = styled.div`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsContainer>
+      <Topics>
+        <Title>TRENDING TOPICS:</Title>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {tabs.map(tab => (
@@ -18,8 +46,8 @@ const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
             selectedTab={selectedTab}
           />
         ))}
-      </div>
-    </div>
+      </Topics>
+    </TabsContainer>
   );
 };
 
