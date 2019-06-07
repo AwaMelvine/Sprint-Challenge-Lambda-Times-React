c3bb5c47de8e7c80612a186264fa9c3992f37a61
Awa Melvine <melvineawa@gmail.com>
2019-06-07 12:01:54 +0100

    Implement carousel component

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,12 +3,14 @@ import TopBar from "./components/TopBar";
 import Header from "./components/Header";
 import Content from "./components/Content/Content";
 import { DefaultStyles } from "./Styled/DefaultStyles";
+import Carousel from "./components/Carousel/Carousel";
 
 const App = () => {
   return (
     <DefaultStyles>
       <TopBar />
       <Header />
+      <Carousel />
       <Content />
     </DefaultStyles>
   );
--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,116 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
-export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
+import React, { Component } from "react";
+import styled from "styled-components";
+import { carouselData } from "../../data";
 
-    }
+const CarouselContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
   }
-  componentDidMount(){
+  .left-button,
+  .right-button {
+    display: flex;
+    justify-content: center;
+    align-items: none;
+    flex-direction: row;
+    color: #fff;
+    background-color: #333;
+    font-size: 40px;
+    border-radius: 50%;
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+  }
+  .left-button:hover,
+  .right-button:hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+  .left-button {
+    top: 50%;
+    left: 25px;
+    transform: translate(0, -50%);
+  }
+  .right-button {
+    top: 50%;
+    right: 25px;
+    transform: translate(0, -50%);
+  }
+  img {
+    width: 100%;
+    display: none;
+  }
+`;
 
+export default class Carousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      slides: [],
+      currentIndex: 0
+    };
+  }
+  componentDidMount() {
+    this.setState({ slides: carouselData });
   }
 
   leftClick = () => {
-
-  }
+    if (this.state.currentIndex === 0) {
+      this.setState({
+        ...this.state,
+        currentIndex: this.state.slides.length - 1
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        currentIndex: this.state.currentIndex - 1
+      });
+    }
+  };
 
   rightClick = () => {
-
-  }
+    if (this.state.currentIndex === this.state.slides.length - 1) {
+      this.setState({
+        ...this.state,
+        currentIndex: 0
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        currentIndex: this.state.currentIndex + 1
+      });
+    }
+  };
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+    const { slides, currentIndex } = this.state;
+
+    return <img src={slides[currentIndex]} style={{ display: "block" }} />;
+  };
+
+  render() {
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
-    )
+      <CarouselContainer>
+        {this.selectedImage()}
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
+      </CarouselContainer>
+    );
   }
 }
