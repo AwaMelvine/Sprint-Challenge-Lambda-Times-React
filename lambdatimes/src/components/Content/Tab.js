5f7f556a19f4a21cc41dd55d2abade47d17cd9b6
Awa Melvine <melvineawa@gmail.com>
2019-06-07 10:53:11 +0100

    Create a styled component for tab component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,47 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const TabContainer = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  ${props =>
+    props.tab === props.selectedTab
+      ? `{
+    background-color: #fff;
+    color: #333;
+    border: 2px solid #333;
+  }`
+      : ``}
+`;
 
 const Tab = ({ tab, selectTabHandler, selectedTab }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  const tabClass = tab === selectedTab ? "tab active-tab" : "tab";
   return (
-    <div className={tabClass} onClick={() => selectTabHandler(tab)}>
+    <TabContainer
+      tab={tab}
+      selectedTab={selectedTab}
+      onClick={() => selectTabHandler(tab)}
+    >
       {tab.toUpperCase()}
-    </div>
+    </TabContainer>
   );
 };
 
