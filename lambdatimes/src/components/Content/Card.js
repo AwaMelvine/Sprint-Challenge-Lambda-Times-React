8abc44a97dcff7489cfd5779c63d03e8e6d4430c
Awa Melvine <melvineawa@gmail.com>
2019-06-07 11:00:54 +0100

    Convert card component to use styled components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,9 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
 
 const Card = ({ card }) => {
   return (
-    <div className="card">
+    <CardContainer>
       <div className="headline">{card.headline}</div>
       <div className="author">
         <div className="img-container">
@@ -11,7 +24,7 @@ const Card = ({ card }) => {
         </div>
         <span>By {card.headline}</span>
       </div>
-    </div>
+    </CardContainer>
   );
 };
 
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -15,42 +15,6 @@ const CardsContainer = styled.div`
   @media (min-width: 1200px) {
     width: 1200px;
   }
-  .card {
-    border-bottom: 1px solid lightgrey;
-    display: flex;
-    justify-content: space-between;
-    align-items: none;
-    flex-direction: column;
-    background-color: #fff;
-    width: 380px;
-    margin-bottom: 16px;
-    padding: 24px;
-  }
-  .card .headline {
-    font-size: 25px;
-    font-family: Didot, serif;
-  }
-  .card .author {
-    display: flex;
-    justify-content: none;
-    align-items: center;
-    flex-direction: row;
-    margin-top: 15px;
-  }
-  .card .author .img-container {
-    padding-right: 10px;
-    border-right: 1px solid lightgrey;
-    height: 40px;
-  }
-  .card .author .img-container img {
-    width: 40px;
-  }
-  .card .author span {
-    padding-left: 10px;
-    font-size: 12px;
-    letter-spacing: 1px;
-    font-weight: bold;
-  }
 `;
 
 const Cards = ({ cards }) => {
